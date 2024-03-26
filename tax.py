ed5be6f13bed870cd8c83b737fa6126924b71cdd
Marian <93202187+bureata@users.noreply.github.com>
2024-03-26 15:22:19 +0200

    - replaced " with ' in the kv file - refactor IncomeWidget to use kv + resize the widget - made IncomeWidget's widgets use properties - add confirmation message for income delete button - add EditIncomePopup - delete comments

--- tax.py
+++ tax.py
@@ -2,12 +2,14 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 from kivy.clock import Clock
+from kivy.properties import ObjectProperty, StringProperty
 
 from kivy.config import Config
 Config.set('graphics', 'width', '1680')
 Config.set('graphics', 'height', '1050')
-Config.set('graphics', 'fullscreen', '0')
+# Config.set('graphics', 'fullscreen', '0')
 
 Config.write()
 # print("Width:", Config.get('graphics', 'width'))
@@ -20,15 +22,8 @@ import sys
 from database import Database
 
 class TaxApp(App):
+
     def build(self):
-        # self.layout = BoxLayout(orientation='vertical')
-        # self.reload_button = Button(text='Reload')
-        # self.reload_button.bind(on_press=self.reload_app)
-        # self.layout.add_widget(self.reload_button)
-        # # Adding MainWindow instance to the layout
-        # self.main_window_layout = MainWindow()
-        # self.layout.add_widget(self.main_window_layout)
-        # return self.layout
     
         return MainWindow()
     
@@ -38,41 +33,93 @@ class TaxApp(App):
         os.execv(sys.executable, ['python'] + sys.argv)
 
 
-class IncomeWidget(BoxLayout):
-    def __init__(self, income_data, **kwargs):
+class DeletePopup(Popup):
+    message_prop = StringProperty()
+    details_prop = StringProperty()
+
+    def __init__(self, message, callback, details=None, **kwargs):
         super().__init__(**kwargs)
-        self.orientation = 'horizontal'
-        self.padding = [10, 10]
-        
-        id_label = Label(text=f"{income_data[0]}")
-        id_label.size_hint = (0.15, 1)
-        id_label.width = 10
-        amount_label = Label(text=f"{income_data[1]}")
-        date_label = Label(text=f"{income_data[2]}")
-        serialnr_label = Label(text=f"{income_data[3]}")
-        
-        self.add_widget(id_label)
-        self.add_widget(amount_label)
-        self.add_widget(date_label)
-        self.add_widget(serialnr_label)
+        self.title = "Confirmation"
+        self.message_prop = message
+        self.details_prop = details
+        self.callback = callback
 
-        edit_button = Button(text="Edit")
-        edit_button.size_hint = (0.5, 1)
-        edit_button.bind(on_press=lambda instance: self.edit_income())
-        self.add_widget(edit_button)
+    def confirm(self):
+        self.callback()
+        self.dismiss()
 
-        delete_button = Button(text="Delete")
-        delete_button.size_hint = (0.5, 1)
-        delete_button.bind(on_press=lambda instance: self.delete_income(income_data))
-        self.add_widget(delete_button)
+
+class EditIncomePopup(Popup):
+    id_prop = StringProperty()
+    amount_prop = StringProperty()
+    date_prop = StringProperty()
+    serialnr_prop = StringProperty()
+
+    def __init__(self, income_data, callback, **kwargs):
+        super(EditIncomePopup, self).__init__(**kwargs)
+        self.callback = callback
+        self.income_data = income_data
+        self.id_prop = str(income_data[0])
+        self.ids.amount_input.text = str(income_data[1])
+        self.ids.date_input.text = str(income_data[2])
+        self.ids.serialnr_input.text = str(income_data[3])
+    
+    def save(self):
+        updated_data = (
+            self.id_prop,
+            self.ids.amount_input.text,
+            self.ids.date_input.text,
+            self.ids.serialnr_input.text
+        )
+        self.callback(updated_data)
+        self.dismiss()
+
+
+class IncomeWidget(BoxLayout):
+    id_prop = StringProperty()
+    amount_prop = StringProperty()
+    date_prop = StringProperty()
+    serialnr_prop = StringProperty()
+
+    def __init__(self, income_data, **kwargs):
+        super(IncomeWidget, self).__init__(**kwargs)
+        self.income_data = income_data
+        self.update_props()
+
+    def update_props(self):
+        self.id_prop = str(self.income_data[0])
+        self.amount_prop = str(self.income_data[1])
+        self.date_prop = str(self.income_data[2])
+        self.serialnr_prop = str(self.income_data[3])
 
     def edit_income(self):
-        pass
+        # method that handles the data returned by the popup
+        def handle_edit(edited_data):
+            if self.income_data != edited_data:
+                self.income_data = edited_data
+                self.update_props()
+                self.parent.parent.parent.db.update_income(*self.income_data)
 
-    def delete_income(self, income_data):
-        # Delete the income record from the database
-        # Implement your logic to delete the record
-        print("Deleting income:", income_data)
+        # Create and open the input popup
+        input_popup = EditIncomePopup(income_data=self.income_data, callback=handle_edit, title='Input Popup')
+        input_popup.open()
+
+    def delete_income(self):
+        def confirmed_deletion():
+            # Delete the income record from the database
+            # Implement your logic to delete the record
+            print(f"Deleting income with id {self.id_prop}:\n{self.income_data}")
+            self.parent.parent.parent.db.del_income(self.income_data[0])
+            self.parent.remove_widget(self)
+        
+        # Create and open the confirmation popup
+        confirmation_popup = DeletePopup(
+            message="Are you sure you want to delete this income?",
+            callback=confirmed_deletion,
+            details=f"This will be deleted from the database:\n{str(self.income_data)}"
+            )
+        confirmation_popup.open()
+
 
 class IncomeLayout(BoxLayout):
     def __init__(self, **kwargs):
@@ -80,15 +127,6 @@ class IncomeLayout(BoxLayout):
         self.orientation = 'vertical'
         self.db = Database()
         Clock.schedule_once(self.update_income_display)
-
-    # def update_income_display(self):
-    #     self.clear_widgets()
-    #     incomes = self.db.get_incomes()
-    #     for income_data in incomes:
-    #         print(income_data)
-    #         custom_income_widget = IncomeWidget(income_data)
-    #         self.add_widget(custom_income_widget)
-
 
     def add_income(self):
         amount = self.ids.amount_input.text
@@ -108,48 +146,17 @@ class IncomeLayout(BoxLayout):
     def nuke(self):
         self.db.nuke_db()
 
-
     def update_income_display(self, dt=None):
-        income_entries_layout = self.ids.income_entries  # Accessing the BoxLayout inside the ScrollView
-        income_entries_layout.clear_widgets()  # Clear existing widgets before adding new ones
-
+        income_entries_layout = self.ids.income_entries
+        income_entries_layout.clear_widgets()
         incomes = self.db.get_incomes()
         for income_data in incomes:
-            custom_income_widget = IncomeWidget(income_data)
+            custom_income_widget = IncomeWidget(income_data=income_data)
             income_entries_layout.add_widget(custom_income_widget)
 
     def nuke(self):
         self.db.nuke_db()
 
-
-# class IncomeLayout(BoxLayout):
-#     def __init__(self, **kwargs):
-#         super(IncomeLayout, self).__init__(**kwargs)
-#         self.db = Database()
-#         # self.update_income_display()
-       
-#     def add_income(self):
-#         amount = self.ids.amount_input.text
-#         date = self.ids.date_input.text
-#         serialnr = self.ids.serialnr_input.text
-#         if serialnr == '':
-#             serialnr = None
-#         self.db.record_income(amount, date, serialnr)
-#         self.update_income_display()
-#         self.clear_input()
-
-#     def update_income_display(self):
-#         incomes = self.db.get_incomes()
-#         incomes_text = '\n'.join(str(income) for income in incomes)
-#         self.ids.incomes_label.text = incomes_text
-
-#     def clear_input(self):
-#         self.ids.amount_input.text = ''
-#         self.ids.date_input.text = ''
-#         self.ids.serialnr_input.text = ''
-
-#     def nuke(self):
-#         self.db.nuke_db()
 
 class ExpenseLayout(BoxLayout):
     def __init__(self, **kwargs):
